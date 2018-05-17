63f0238ac1d621e85e9a75aee305efdce5538e36
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2018-05-17 00:37:34 -0400

    playing around with checkboxes

--- screens/Home.js
+++ screens/Home.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native'
 import { CheckBox } from 'react-native-elements'
 
 export default class Home extends React.Component {
-  state = {
-    checked: false
+  constructor(props) {
+    super(props)
+    this.state = {
+      checked: false
+    }
   }
 
   handleCheckbox = () => {
     this.setState({ checked: !this.state.checked })
   }
   render() {
+    console.log(this.state.checked)
     return (
       <View style={styles.container}>
         <View style={styles.headerContainer}>
@@ -19,27 +23,31 @@ export default class Home extends React.Component {
         <View>
           <View style={styles.welcomeContainer}>
           <View style={styles.row}>
-            <Text style={styles.welcomeText}>Hello, </Text><Text style={[styles.bold, styles.welcomeText]}>IJEMMA</Text>
+            <Text style={styles.welcomeText}>Hello, </Text><Text style={[styles.bold, styles.welcomeText]}>IJEMMA!</Text>
           </View>
             <Text style={styles.welcomeText}>Sat, May 12</Text>
             <Text style={styles.welcomeText}>72 F</Text>
           </View>
           <View>
             <Text>I'm thirsty</Text>
+            <Image
+              style={styles.plant}
+              source={require('./../assets/images/plant.png')}
+            />
           </View>
           <View style={styles.checkItemsContainer}>
             <FlatList
               data={[
                 { key: 'a', time: '8 AM', reminder: 'Apply sunscreen' },
                 { key: 'b', time: '9 AM', reminder: 'Drink water' },
-                { key: 'c', time: '11 AM', reminder: 'Take meds' }]}
+                { key: 'c', time: '11 AM', reminder: 'Take meds' }
+              ]}
               renderItem={({ item }) => {
                 return (
                   <View style={styles.checkContainer}>
                     <CheckBox
-                      center
                       checked={this.state.checked}
-                      onPress={this.handedCheckbox}
+                      onPress={() => this.handleCheckbox()}
                       containerStyle={styles.checkboxContainer}
                     />
                     <Text style={styles.bold}>{item.time} </Text>
@@ -77,6 +85,10 @@ const styles = StyleSheet.create({
     marginTop: 5,
     marginRight: 15,
   },
+  plant: {
+    alignSelf: 'flex-start',
+    marginLeft: 30,
+  },
   checkItemContainer: {
     flexDirection: 'column',
     alignItems: 'center',
@@ -93,12 +105,15 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
   },
   checkboxContainer: {
-    backgroundColor: 'transparent',
-    borderColor: 'transparent',
-    width: 40,
+    backgroundColor: '#F4FCFF',
+    borderColor: '#B2CBFB',
+    borderWidth: 2,
+    width: 20,
+    height: 20,
   },
   welcomeText: {
     fontSize: 18,
+    marginBottom: 5,
   },
   row: {
     flexDirection: 'row',
