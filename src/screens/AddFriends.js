a07ce5ba666328fc9ae9a37ad72ad7c32cd1abcf
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2018-05-19 16:31:32 -0400

    included username

--- src/screens/AddFriends.js
+++ src/screens/AddFriends.js
@@ -22,6 +22,7 @@ export default class AddFriends extends React.Component {
     }
   }
   renderSearchedUsers = () => {
+    const { navigate } = this.props.navigation
     if (this.state.searchedUsers) {
       return (
         <FlatList
@@ -29,17 +30,18 @@ export default class AddFriends extends React.Component {
           style={{ height: '100%' }}
           renderItem={({ item }) => {
             return (
-              // <TouchableOpacity
-              //   onPress={() => navigate('IndividualFriend', { name: item.name, username: item.username })}
-              // >
-              <View style={styles.friendContainer}>
-                <Image
-                  style={styles.animal}
-                  source={require('./../assets/images/plantcircle.png')}
-                />
-                <Text style={styles.bold}>{item.name.toUpperCase()}</Text>
-              </View>
-              // </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => navigate('IndividualFriend', { name: item.name, username: item.username })}
+              >
+                <View style={styles.friendContainer}>
+                  <Image
+                    style={styles.animal}
+                    source={require('./../assets/images/plantcircle.png')}
+                  />
+                  <Text style={styles.bold}>{item.name.toUpperCase()}</Text>
+                  <Text>@{item.username}</Text>
+                </View>
+              </TouchableOpacity>
             )
           }
           }
@@ -52,16 +54,22 @@ export default class AddFriends extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput
-          autoCapitalize="none"
-          placeholder="Search for Friends"
-          onChangeText={text => this.handleSearch(text)}
-          value={this.state.searchText}
-          style={styles.searchBar}
-        />
-        <TouchableOpacity onPress={this.searchForUsers}>
-          <Text>Search</Text>
-        </TouchableOpacity>
+        <View style={styles.searchBarContainer}>
+          <TextInput
+            autoCapitalize="none"
+            placeholder="Search for Friends"
+            onChangeText={text => this.handleSearch(text)}
+            value={this.state.searchText}
+            style={styles.searchBar}
+          />
+          <TouchableOpacity onPress={this.searchForUsers}>
+            <Image
+              style={styles.searchIcon}
+              source={require('./../assets/images/search.png')}
+            />
+            {/* <Text>Search</Text> */}
+          </TouchableOpacity>
+        </View>
         {this.renderSearchedUsers()}
       </View>
     )
@@ -73,14 +81,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF5E7',
     height: '100%',
   },
+  searchBarContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   searchBar: {
     alignSelf: 'center',
-    width: '90%',
+    width: '80%',
     fontSize: 22,
     color: '#053867',
     marginTop: 20,
+    paddingBottom: 5,
     borderBottomWidth: 2,
     borderBottomColor: '#053867',
+  },
+  searchIcon: {
+    height: 30,
+    width: 30,
+    marginTop: 20,
+    marginLeft: 10,
   },
   friendContainer: {
     flexDirection: 'row',
