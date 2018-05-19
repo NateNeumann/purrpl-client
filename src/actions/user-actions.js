9276144bdf26df700d58fd5eb3889b3a002d3127
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2018-05-19 16:11:14 -0400

    able to search for users on friend

--- src/actions/user-actions.js
+++ src/actions/user-actions.js
@@ -3,10 +3,19 @@ import axios from 'axios'
 // const ROOT_URL = 'https://project-api-black-mirror.herokuapp.com/api'
 const ROOT_URL = 'http://localhost:9090/api'
 
-export default function fetchUsers(searchTerm) {
+export function fetchUsers() {
   return new Promise((resolve, reject) => {
-    axios.get(`${ROOT_URL}/users`, { search: searchTerm }).then((response) => {
-      console.log(response.data)
+    axios.get(`${ROOT_URL}/users`).then((response) => {
+      resolve(response.data)
+    }).catch((error) => {
+      reject(error)
+    })
+  })
+}
+
+export function fetchSearchedUsers(searchTerm) {
+  return new Promise((resolve, reject) => {
+    axios.get(`${ROOT_URL}/users/${searchTerm}`).then((response) => {
       resolve(response.data)
     }).catch((error) => {
       reject(error)
--- src/screens/AddFriends.js
+++ src/screens/AddFriends.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { StyleSheet, View, TextInput } from 'react-native'
-import fetchUsers from './../actions/user-actions'
+import { StyleSheet, View, TextInput, TouchableOpacity, Text, FlatList, Image } from 'react-native'
+import { fetchSearchedUsers } from './../actions/user-actions'
 
 export default class AddFriends extends React.Component {
   constructor(props) {
@@ -8,17 +8,50 @@ export default class AddFriends extends React.Component {
 
     this.state = {
       searchText: '',
+      searchedUsers: [],
     }
   }
   handleSearch = (text) => {
     this.setState({ searchText: text })
-    fetchUsers(text).then((response) => {
-      console.log('out here', response)
-    })
+  }
+  searchForUsers = () => {
+    if (this.state.searchText) {
+      fetchSearchedUsers(this.state.searchText).then((response) => {
+        this.setState({ searchedUsers: response })
+      })
+    }
+  }
+  renderSearchedUsers = () => {
+    if (this.state.searchedUsers) {
+      return (
+        <FlatList
+          data={this.state.searchedUsers}
+          style={{ height: '100%' }}
+          renderItem={({ item }) => {
+            return (
+              // <TouchableOpacity
+              //   onPress={() => navigate('IndividualFriend', { name: item.name, username: item.username })}
+              // >
+              <View style={styles.friendContainer}>
+                <Image
+                  style={styles.animal}
+                  source={require('./../assets/images/plantcircle.png')}
+                />
+                <Text style={styles.bold}>{item.name.toUpperCase()}</Text>
+              </View>
+              // </TouchableOpacity>
+            )
+          }
+          }
+        />
+      )
+    } else {
+      return <Text>No users found :(</Text>
+    }
   }
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <TextInput
           autoCapitalize="none"
           placeholder="Search for Friends"
@@ -26,12 +59,20 @@ export default class AddFriends extends React.Component {
           value={this.state.searchText}
           style={styles.searchBar}
         />
+        <TouchableOpacity onPress={this.searchForUsers}>
+          <Text>Search</Text>
+        </TouchableOpacity>
+        {this.renderSearchedUsers()}
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFF5E7',
+    height: '100%',
+  },
   searchBar: {
     alignSelf: 'center',
     width: '90%',
@@ -40,5 +81,21 @@ const styles = StyleSheet.create({
     marginTop: 20,
     borderBottomWidth: 2,
     borderBottomColor: '#053867',
+  },
+  friendContainer: {
+    flexDirection: 'row',
+    padding: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  animal: {
+    alignSelf: 'flex-start',
+  },
+  bold: {
+    fontFamily: 'raleway-bold',
+    fontSize: 20,
+    paddingLeft: 20,
+    marginTop: 20,
+    color: '#053867',
   },
 })
