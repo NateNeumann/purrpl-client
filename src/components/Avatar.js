be4029c5791222b2df23e6e0c4e30554c30b7a3c
Raul Rodriguez <rrod.19@dartmouth.edu>
2018-05-25 00:02:11 -0400

    replacing const fot states

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -7,20 +7,12 @@ export default class Avatar extends React.Component {
 
     this.state = {
       happy: require('./../assets/images/cat/cat_1.png'),
+      sad: require('./../assets/images/cat/cat_4.png'),
+      deprived: require('./../assets/images/cat/cat_5.png'),
+      rollover: require('./../assets/images/cat/cat_8.png'),
     }
   }
-
   render() {
-    const mood = [{
-      happy: require('./../assets/images/cat/cat_1.png'),
-    }, {
-      rollover: require('./../assets/images/cat/cat_8.png'),
-    }, {
-      deprived: require('./../assets/images/cat/cat_5.png'),
-    }, {
-      sad: require('./../assets/images/cat/cat_4.png'),
-    }];
-
     return (
       <Image
         style={styles.animal}
