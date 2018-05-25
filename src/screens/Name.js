cf1f80d32cba40a66531178c070d6a1b7dc87f90
Sofia Stanescu-Bellu <sofiastb27@gmail.com>
2018-05-24 21:02:55 -0400

    Add back button and styling

--- src/screens/Name.js
+++ src/screens/Name.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Image, Text, TouchableOpacity, TextInput, Alert } from 'react-native'
 import { LinearGradient } from 'expo'
+import Back from './../components/Back'
 
 export default class Name extends Component {
   static navigationOptions = { header: null }
@@ -34,6 +35,9 @@ export default class Name extends Component {
           colors={['#420A75', '#5B1997']}
           style={styles.gradient}
         >
+          <View style={styles.backView}>
+            <Back style={styles.backButton} navigation={this.props.navigation} />
+          </View>
           <View style={styles.content}>
             <Image style={styles.cat} source={require('../assets/images/light_purple_cat.png')} />
             <Text style={styles.nameText}>What&#39;s your <Text style={styles.bold}>name?</Text></Text>
@@ -60,8 +64,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     width: '100%',
   },
+  backView: {
+    alignItems: 'flex-start',
+    flex: 1,
+    justifyContent: 'flex-end',
+    width: '100%',
+  },
+  backButton: {
+    alignSelf: 'flex-end',
+  },
   content: {
     alignItems: 'center',
+    flex: 10,
     justifyContent: 'center',
   },
   cat: {
