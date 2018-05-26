6a6cbeba6910732f775f9208eb62d21a7d215483
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2018-05-26 16:40:38 -0400

    toggles notifications and shows correct state from backend

--- src/actions/user-actions.js
+++ src/actions/user-actions.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const ROOT_URL = 'https://project-api-black-mirror.herokuapp.com/api'
+// const ROOT_URL = 'https://project-api-black-mirror.herokuapp.com/api'
+const ROOT_URL = 'http://localhost:9090/api'
 
 export function createUser(user) {
   return new Promise((resolve, reject) => {
@@ -35,6 +36,16 @@ export function fetchUsers() {
 export function fetchSearchedUsers(id, searchTerm) {
   return new Promise((resolve, reject) => {
     axios.get(`${ROOT_URL}/users/${id}&${searchTerm}`).then((response) => {
+      resolve(response.data)
+    }).catch((error) => {
+      reject(error)
+    })
+  })
+}
+
+export function toggleNotifications(id, active) {
+  return new Promise((resolve, reject) => {
+    axios.put(`${ROOT_URL}/user/notifications/${id}`, { active }).then((response) => {
       resolve(response.data)
     }).catch((error) => {
       reject(error)
