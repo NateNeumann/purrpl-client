86949247302f03dbc96e6e0c0bc4e9d30e428e39
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2018-05-29 01:39:30 -0400

    fixed changes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -32,7 +32,7 @@ export default class Home extends React.Component {
     getWeather(43.7005122, -72.2839756).then((response) => {
       this.setState({ weather: response })
     })
-
+    
     this.updateReminders()
   }
 
