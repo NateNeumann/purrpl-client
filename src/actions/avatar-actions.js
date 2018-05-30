f1f677b8a1dfd01d62e70de798a6caebe1a98da2
Sofia Stanescu-Bellu <sofiastb27@gmail.com>
2018-05-30 13:34:39 -0400

    Change API route to heroku

--- src/actions/avatar-actions.js
+++ src/actions/avatar-actions.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const ROOT_URL = 'https://project-api-black-mirror.herokuapp.com/api'
-const ROOT_URL = 'http://localhost:9090/api'
+const ROOT_URL = 'https://project-api-black-mirror.herokuapp.com/api'
+// const ROOT_URL = 'http://localhost:9090/api'
 
 // id is user id
 export default function getAvatar(id) {
