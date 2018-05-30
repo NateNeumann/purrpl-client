f49ea75538547b601bf2f042068bf88f82c64ffc
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2018-05-29 22:06:34 -0400

    able to swipe back

--- src/components/SlideMenu.js
+++ src/components/SlideMenu.js
@@ -24,48 +24,26 @@ export default class SlideMenu extends React.Component {
     })
 
     this._panResponder = PanResponder.create({
-      // Ask to be the responder:
       onStartShouldSetPanResponder: (evt, gestureState) => true,
       onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
       onMoveShouldSetPanResponder: (evt, gestureState) => true,
       onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
-
-      onPanResponderGrant: (evt, gestureState) => {
-        // The gesture has started. Show visual feedback so the user knows
-        // what is happening!
-
-        // gestureState.d{x,y} will be set to zero now
-        console.log('start')
-      },
-      onPanResponderMove: (evt, gestureState) => {
-        // The most recent move distance is gestureState.move{X,Y}
-
-        // The accumulated gesture distance since becoming responder is
-        // gestureState.d{x,y}
-        console.log('move')
-      },
       onPanResponderTerminationRequest: (evt, gestureState) => true,
       onPanResponderRelease: (evt, gestureState) => {
-        // The user has released all touches while this view is the
-        // responder. This typically means a gesture has succeeded
-        console.log('release')
         if (gestureState.dx < 0) {
-          console.log('swiping back')
-          let toValue = -100
-          this.props.toggleMenu()
-          if (this.props.visible) {
-            toValue = 0
-          }
+          const toValue = -300
           Animated.spring(
             this.state.bounceValue,
             {
               toValue,
-              tension: 2,
+              tension: 3,
             },
-          ).start();
+          ).start(() => {
+            this.props.toggleMenu()
+          })
         }
       },
-    });
+    })
   }
 
   render() {
